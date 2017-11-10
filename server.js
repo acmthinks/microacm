0ea3dbbd578b97d40530239f88b74090eda39ea4
acm1 <acm@us.ibm.com>
2017-11-09 22:06:37 -0600

    package dependency updates

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
